036a5cb819c7a31887aaa049227c525343c975c6
anzep <anze.pureber@gmail.com>
2019-10-23 10:22:21 +0200

    data shows, ugly code, no pic

--- pages/index.js
+++ pages/index.js
@@ -4,18 +4,38 @@ import fetch from 'isomorphic-fetch';
 
 function getShows() {
   return fetch('https://api.infinum.academy/api/shows')
-    .then((res) => res.json());
+    .then((response) => response.json());
 }
 function Index() {
   const {value} = useAsync(getShows);
 
   return (
     <div>
-      <h1>{value && value.data[0].title}</h1>
-      {value && value.data[0]._id}
-      {value && value.data[0].imageUrl}
-      {value && value.data[0].likesCount}
-      <h1>hi</h1>
+      <div>
+        {value && value.data[0].title}
+        {value && value.data[0].imageUrl}
+        {value && value.data[0].likesCount}
+      </div>
+      <div>
+        {value && value.data[1].title}
+        {value && value.data[1].imageUrl}
+        {value && value.data[1].likesCount}
+      </div>
+      <div>
+        {value && value.data[2].title}
+        {value && value.data[2].imageUrl}
+        {value && value.data[2].likesCount}
+      </div>
+      <div>
+        {value && value.data[3].title}
+        {value && value.data[3].imageUrl}
+        {value && value.data[3].likesCount}
+      </div>
+      <div>
+        {value && value.data[4].title}
+        {value && value.data[4].imageUrl}
+        {value && value.data[4].likesCount}
+      </div>
     </div>
   );
 }
