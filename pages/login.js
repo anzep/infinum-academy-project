29bc59ed5afa358982b96b6b2d1faf6915869e2c
anzep <anze.pureber@gmail.com>
2019-10-27 23:56:39 +0100

    added images to public, created and linked temporary header component

---
+++ pages/login.js
@@ -0,0 +1,19 @@
+import Header from '../components/header-logo';
+
+function Login(){
+
+    return (
+        <div>
+            <div>
+                {Header()}
+            </div>
+            <div>
+                body
+            </div>
+            <div>
+                footer
+            </div>
+        </div>
+    );
+}
+export default Login;
---
+++ pages/register.js
@@ -0,0 +1,14 @@
+function Register(){
+
+    return (
+        <div>
+            <div>
+                header
+            </div>
+            <div>
+                body
+            </div>
+        </div>
+    );
+}
+export default Register;
