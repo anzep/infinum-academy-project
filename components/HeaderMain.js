5eba53187142bd2c1821c66991bc946dbd62e509
anzep <anze.pureber@gmail.com>
2019-10-30 16:49:17 +0100

    added two new components for main footer and header, created shows page

---
+++ components/HeaderMain.js
@@ -0,0 +1,27 @@
+import { css } from '@emotion/core';
+
+const headerMain = css `
+  display: flex;
+  justify-content: space-around;
+  align-items: center;
+
+  font-family: 'Verdana';
+`;
+const headerImg = css`
+  height: 20px;
+`;
+
+function HeaderMain() {
+  return (
+    <div css={headerMain}>
+      <div>
+        <img src='img-logo-horizontal@3x.png' alt='page logo' css={headerImg} />
+      </div>
+      <div>
+        <p>Welcome!</p>
+      </div>
+    </div>
+  );
+}
+
+export default HeaderMain;
---
+++ pages/shows.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import { css } from '@emotion/core';
+import HeaderMain from '../components/HeaderMain';
+
+function Shows() {
+
+  return (
+    <div>
+      <HeaderMain />
+    </div>
+  );
+}
+
+export default Shows;
